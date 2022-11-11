bb2b700d2edec541f0958ee7d02673ebe1f43b9f
zhihan <zhihan6@stanford.edu>
2022-11-11 08:24:16 -0800

    Implement the Complete and Exit button.

--- gui.py
+++ gui.py
@@ -3,14 +3,24 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
-
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
+
+
+# Add
+add_button = sg.Button("Add")
+# Edit
 edit_button = sg.Button("Edit")
+# Complete
+complete_button = sg.Button("Complete")
+# Exit
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -33,8 +43,19 @@ while True:
         functions.write_todos(todos)
         window['todos'].update(values=todos)
 
+    elif event == 'Complete':
+        todo_to_complete = values['todos'][0]
+        todos = functions.get_todos()
+        todos.remove(todo_to_complete)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value='')
+
     elif event == 'todos':
         window['todo'].update(value=values['todos'][0])
+
+    elif event == 'Exit':
+        break
 
     elif event == sg.WINDOW_CLOSED:
         break
